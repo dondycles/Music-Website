4134ddf6b16f55c73c72aacc17a71fe42b9f90b9
dondycles <johnrod.dondoyano@gmailcomm>
2023-08-22 23:15:45 +0800

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
    
     Initial commit
    
     Changes to be committed:
    	new file:   .vscode/settings.json
    	new file:   albums.html
    	new file:   debug.log
    	new file:   imgs/580b57fcd9996e24bc43c51b.png
    	new file:   imgs/58482c44cef1014c0b5e4a3c.png
    	new file:   imgs/88130c7d84527633635595b3d8547177 (1).png
    	new file:   imgs/AppleMusic-logo-white.png
    	new file:   imgs/Bb.png
    	new file:   imgs/Dondy.jpg
    	new file:   imgs/Imahinasyon.png
    	new file:   imgs/Silhouette Art.png
    	new file:   imgs/Silhouette Wallpaper.png
    	new file:   imgs/Spotify Cover.jpg
    	new file:   imgs/Walking copy.png
    	new file:   imgs/Walking.png

---
+++ script.js
@@ -0,0 +1,78 @@
+const coll = document.getElementsByClassName("collapsible");
+const audio = document.querySelector("audio");
+
+for (var i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight){
+          content.style.maxHeight = null;
+      } 
+      else {
+          content.style.maxHeight = content.scrollHeight + "px";
+      } 
+
+    });
+}
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  function myFunctionYT() {
+    document.getElementById("myDropdownYT").classList.toggle("show");
+  }
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-contentYT");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  function myFunctionF() {
+    document.getElementById("myDropdownF").classList.toggle("show");
+  }
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-contentF");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  window.onclick = function(event){
+    if (event.target.matches('.playmusic')) {
+      window.addEventListener("click", event => {
+        audio.volume = 0.2;
+        audio.play();
+      });
+    }
+
+  }
+  
